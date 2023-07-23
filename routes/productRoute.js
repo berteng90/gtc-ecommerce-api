329c3823df4fe8295712f0fdd2a1c85b80e844c9
unknown <berteng90@gmail.com>
2023-07-23 16:29:05 +0800

    Modified Inventory API Query

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -90,14 +90,16 @@ productRouter.get("/name/:name", async (req, res) => {
   const request = new sql.Request();
   request.input("name", sql.VarChar, name);
   const query = request.query(
-    "SELECT * FROM API_Item_InventoryMaster WHERE Name LIKE '%' + @name + '%'"
+    "SELECT Item_InventoryMaster.ItemId as [itemID],ItemDescription as [itemName],CategoryCode as [categoryCode],UOMCode as [uomCode],  SUM(QtyIN-QtyOUT) as [stockCount], Price2 as [retailPrice], PictPath as [imagePath] FROM Item_InventoryMaster INNER JOIN Item_InvoiceBatchDetailed ON Item_InvoiceBatchDetailed.ItemID=Item_InventoryMaster.ItemID INNER JOIN Item_MeasurableUOM ON Item_MeasurableUOM.ItemID=Item_InventoryMaster.ItemID AND Item_MeasurableUOM.Branch=15 INNER JOIN Item_RelationUOM ON Item_RelationUOM.MeasureID=Item_MeasurableUOM.MeasureID AND Item_RelationUOM.Branch=Item_MeasurableUOM.Branch INNER JOIN Item_UOMMaster ON Item_UOMMaster.UOMID=Item_RelationUOM.UOMId WHERE Item_InvoiceBatchDetailed.Branch=15 AND ItemDescription like '%' + @name + '%'  GROUP BY Item_InventoryMaster.ItemId,ItemDescription,UOMCode, Price2, CategoryCode, PictPath"
   );
   const data = (await query).recordset.map((item) => ({
-    ID: item.itemID,
-    ItemName: item.Name,
-    ItemDescription: item.Description,
-    ItemPrice: item.Price,
-    ItemImage: item.Image,
+    itemID: item.itemID,
+    itemName: item.itemName,
+    itemCategoryCode: item.categoryCode,
+    itemUomCode: item.uomCode,
+    itemStock: item.stockCount,
+    itemPrice: item.retailPrice,
+    itemImagePath: item.imagePath,
   }));
   res.status(200).send(data);
   sql.close();
