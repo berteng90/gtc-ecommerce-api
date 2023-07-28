b7f1a5b0214c94b61831197df2398e57021b58f6
unknown <berteng90@gmail.com>
2023-07-28 12:01:41 +0800

    Modified Order Number Generation

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -7,34 +7,17 @@ transactionRouter.get("/reference", async (req, res) => {
   await connect();
   const request = new sql.Request();
   const query = request.query(
-    "SELECT MAX(SalesDocNumber)+1 as [SalesDocNumber] FROM Item_Invoice WHERE SalesType='Cash Sales'"
+    "SELECT 'I' + CAST(MAX(CAST(RIGHT(ReferenceNumber, 6) AS INT)) + 1 AS VARCHAR(7)) as [referenceNumber] FROM Item_SalesOrder"
   );
   const response = (await query).recordset.map((reference) => ({
-    InvoiceNumber: reference.SalesDocNumber,
+    orderNumber: reference.referenceNumber,
   }));
-  const currSalesDocNumber = response[0].InvoiceNumber.toString();
-
-  switch (currSalesDocNumber.length) {
-    case 6:
-      response[0].InvoiceNumber = "0" + currSalesDocNumber;
-      break;
-    case 5:
-      response[0].InvoiceNumber = "00" + currSalesDocNumber;
-      break;
-    case 4:
-      response[0].InvoiceNumber = "000" + currSalesDocNumber;
-      break;
-    case 3:
-      response[0].InvoiceNumber = "0000" + currSalesDocNumber;
-      break;
-    case 2:
-      response[0].InvoiceNumber = "00000" + currSalesDocNumber;
-      break;
-    case 1:
-      response[0].InvoiceNumber = "000000" + currSalesDocNumber;
-      break;
-  }
   res.status(200).send(response);
+  sql.close();
+});
+
+transactionRouter.get("/checkout", async (req, res) => {
+  res.status(200).send("Hello World");
 });
 
 module.exports = transactionRouter;
